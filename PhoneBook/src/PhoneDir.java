3ca233f3be1808b7f67f9e469ba552d4ddc8039d
John W. Martin <36454057+jmartin103@users.noreply.github.com>
2019-01-03 14:29:20 -0500

    Clean up code

--- PhoneBook/src/PhoneDir.java
+++ PhoneBook/src/PhoneDir.java
@@ -2,7 +2,7 @@ import java.util.Collections;
 import java.util.Scanner;
 import java.util.Vector;
 
-/*
+/**
  * This program simulates a phone directory to store first names, last names, 
  * and phone numbers. It allows the user to add, delete, and change records
  * within the directory. A user can change a first name, a last name, or a 
@@ -25,9 +25,9 @@ public class PhoneDir {
 	// PhoneBook where all entries will be contained
 	static Vector<PhoneRecord> phoneBook = new Vector<PhoneRecord>();
 	static Scanner sc = new Scanner(System.in); // Scanner to read user input
-	static PhoneRecord currRecord = null; // Current record is null
+	static PhoneRecord currRecord = null;
 	
-	// Menu of options
+	/** Menu of options */
 	public static void menu() {
 		System.out.println("a - Show all records\n"
 				+ "d - Delete the current record\n"
@@ -40,65 +40,67 @@ public class PhoneDir {
 				+ " record");
 	}
 	
-	// Sort the directory accordingly
+	/** Sort the directory in ascending alphabetical order. */
 	public static void sortDirectory() {
 		Collections.sort(phoneBook);
 	}
 	
-	// Display current record
+	/** Display current record if one is selected. */
 	public static void displayCurrentRecord() {
 		if (currRecord == null) { // No current record is selected
 			System.out.println("No current record selected");
 		}
-		else { // Display current record with first and last name and phone number
+		else {
 			System.out.println("Current record: " + currRecord.toString());
 		}
 	}
 	
-	// Display all records with first name, last name, and phone number
+	/** Display all records with first name, last name, and phone number. */
 	public static void displayAllRecords() {
-		if (phoneBook.isEmpty()) { // Directory is empty
+		if (phoneBook.isEmpty()) {
 			System.out.println("Directory is empty!");
 		}
 		else {
 			for (int i = 0; i < phoneBook.size(); i++) {
 				System.out.println(phoneBook.get(i).toString());
 			}
-			sortDirectory(); // Maintain sorting
-			displayCurrentRecord(); // Display current record
+			sortDirectory(); 
+			displayCurrentRecord(); 
 		}
 	}
 	
-	// Add a new record to the directory
+	/** Add a new record to the directory. When a new record is added, it becomes
+	 *  the current record, and sorting is maintained.
+	 */
 	public static void addRecord() {
-		// Prompt user for first name
 		System.out.print("Please enter the first name for the record: ");
 		String fName = sc.next();
-		
-		// Prompt user for last name
+
 		System.out.print("Please enter the last name for the record: ");
 		String lName = sc.next();
-		
-		// Prompt user for phone number
+
 		System.out.print("Please enter the phone number for the record: ");
 		String phoneNum = sc.next();
-		
-		// Create new record
+
 		PhoneRecord newRecord = new PhoneRecord(fName, lName, phoneNum);
 		
-		phoneBook.add(newRecord); // Add record to directory
-		sortDirectory(); // Maintain sorting
-		currRecord = newRecord; // Set the current record to the new record
-		displayCurrentRecord(); // Display current record
+		phoneBook.add(newRecord);
+		sortDirectory();
+		currRecord = newRecord;
+		displayCurrentRecord();
 	}
 	
-	// Method to delete the current record
+	/** Delete the current record. When a record is deleted, no current record is
+	 *  selected.
+	 *  @return false if directory is empty
+	 *  @return true otherwise
+	 */
 	public static boolean deleteRecord() {
-		try {
-			phoneBook.remove(currRecord); // Delete the current record
+		try { // Attempt to remove the current record
+			phoneBook.remove(currRecord);
 			System.out.println("Deleted: " + currRecord.toString());
-			currRecord = null; // Set current record to null
-			sortDirectory(); // Maintain sorting
+			currRecord = null;
+			sortDirectory();
 			displayCurrentRecord();
 		}
 		catch (NullPointerException e) { // No records in directory
@@ -109,71 +111,93 @@ public class PhoneDir {
 		return true; // Successful deletion
 	}
 	
-	// Method to change the first name of the current record
+	/** Method to change the first name in the current record. A current record 
+	 *  must be selected in order for this to happen. If no current record is 
+	 *  selected, then a message is printed, telling the user to select a 
+	 *  current record before the first name can be changed. Otherwise, the 
+	 *  user is prompted for the new first name of the record. The record is 
+	 *  then modified and sorted accordingly.
+	 */
 	public static void changeFirstName() {
-		if (currRecord == null) { // No current record is selected
+		if (currRecord == null) {
 			System.out.println("No current record; please select record first");
 		}
-		else { // Current record is selected; prompt user for new first name
+		else {
 			System.out.print("Please enter the new first name for the record: ");
-			String firstName = sc.next(); // Read first name
-			currRecord.setFirstName(firstName); // Set first name in current record
-			sortDirectory(); // Maintain sorting
+			String firstName = sc.next();
+			currRecord.setFirstName(firstName);
+			sortDirectory();
 			displayCurrentRecord();
 		}
 	}
 	
-	// Method to change the last name of the current record
+	/** Method to change the last name in the current record. A current record 
+	 *  must be selected in order for this to happen. If no current record is 
+	 *  selected, then a message is printed, telling the user to select a 
+	 *  current record before the last name can be changed. Otherwise, the 
+	 *  user is prompted for the new last name of the record. The record is 
+	 *  then modified and sorted accordingly.
+	 */
 	public static void changeLastName() {
-		if (currRecord == null) { // No current record is selected
+		if (currRecord == null) {
 			System.out.println("No current record; please select record first");
 		}
-		else { // Current record is selected; prompt user for new last name
+		else { 
 			System.out.print("Please enter the new last name for the record: ");
-			String lastName = sc.next(); // Read last name
-			currRecord.setLastName(lastName); // Set last name in current record
-			sortDirectory(); // Maintain sorting
+			String lastName = sc.next();
+			currRecord.setLastName(lastName);
+			sortDirectory();
 			displayCurrentRecord();
 		}
 	}
 	
-	// Method to change the phone number of the current record
+	/** Method to change the phone number in the current record. A current record 
+	 *  must be selected in order for this to happen. If no current record is 
+	 *  selected, then a message is printed, telling the user to select a 
+	 *  current record before the phone number can be changed. Otherwise, the 
+	 *  user is prompted for the new phone number of the record. The record is 
+	 *  then modified and sorted accordingly.
+	 */
 	public static void changePhoneNumber() {
-		if (currRecord == null) { // No current record is selected
+		if (currRecord == null) {
 			System.out.println("No current record; please select record first");
 		}
-		else { // Current record is selected; prompt user for new phone number
+		else {
 			System.out.print("Please enter the new phone number for the record: ");
-			String phoneNo = sc.next(); // Read phone number
-			currRecord.setPhoneNumber(phoneNo); // Set phone number in current record
-			sortDirectory(); // Maintain sorting
+			String phoneNo = sc.next();
+			currRecord.setPhoneNumber(phoneNo);
+			sortDirectory();
 			displayCurrentRecord();
 		}
 	}
 	
-	// Select a new record to become the current record
+	/** Select a new record to become the current record. The user is prompted for 
+	 *  the first and last name of the record that they want to find to become 
+	 *  the new current record. The method searches the directory for the record, 
+	 *  and if there is a matching record, that record becomes the new current 
+	 *  record.
+	 */
 	public static void selectRecord() {
-		if (phoneBook.isEmpty()) { // Directory is empty
+		if (phoneBook.isEmpty()) {
 			System.out.println("Directory is empty!");
 		}
-		else { // Prompt user for first name and last name
+		else {
 			System.out.print("Enter first name: ");
 			String firstName = sc.next();
 			System.out.print("Enter last name: ");
 			String lastName = sc.next();
 			
-			boolean found = false; // Initialize found to false
+			boolean found = false;
 			
-			// Iterate through the directory for the first name and last name
 			for (int i = 0; i < phoneBook.size(); i++) {
 				if (firstName.equalsIgnoreCase(phoneBook.get(i).getFirstName()) 
 					&& lastName.equalsIgnoreCase(phoneBook.get(i).getLastName())) {
-					found = true; // Record is found in directory
-					currRecord = phoneBook.get(i); // Set current record				
+					found = true;
+					currRecord = phoneBook.get(i);			
 				}
 			}
 			
-			if (!found) { // User-given record not found in directory
+			if (!found) {
 				System.out.println("No matching record found!");
 			}
 			
@@ -181,47 +205,47 @@ public class PhoneDir {
 		}
 	}
 	
-	// Terminate program
+	/** Terminate program. */
 	public static void terminate() {
 		System.exit(0);
 	}
 	
-	// Main method, where the user will interact with the program
+	/** Main method, where the user will interact with the program. */
 	public static void main(String[] args) {	
 		while (true) {
 			menu(); // Print menu of options
 			
 			// Prompt user for option from the menu
 			System.out.print("Please select an option from the menu: ");
-			char choice = sc.next().charAt(0); // Read option
+			char choice = sc.next().charAt(0);
 			
 			// Execute the option given by the user
 			switch (choice) {
-			case 'a': case 'A': // Display all records in directory
+			case 'a': case 'A':
 				displayAllRecords();
 				break;
-			case 'd': case 'D': // Delete the current record
+			case 'd': case 'D':
 				deleteRecord();
 				break;
-			case 'f': case 'F': // Change the first name in the current record
+			case 'f': case 'F':
 				changeFirstName();
 				break;
-			case 'l': case 'L': // Change the last name in the current record
+			case 'l': case 'L':
 				changeLastName();
 				break;
-			case 'n': case 'N': // Add a new record to the directory
+			case 'n': case 'N':
 				addRecord();
 				break;
-			case 'p': case 'P': // Change the phone number in the current record
+			case 'p': case 'P':
 				changePhoneNumber();
 				break;
-			case 'q': case 'Q': // Terminate program
+			case 'q': case 'Q': 
 				terminate();
 				break;
-			case 's': case 'S': // Select new record to become current record
+			case 's': case 'S': 
 				selectRecord();
 				break;
-			default: // User entered option not in menu; prompt for re-entry
+			default:
 				System.out.println("Invalid option; please try again");
 			}
 		}
